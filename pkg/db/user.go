ecaeb3f9c8245b31e7642e5365212312b5061eeb
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-07-26 18:30:07 -0400

    feat(user): Se agrega funciones para poder crear un usuario y autenticarlo correctamente, con contraseñas hasheadas

---
+++ pkg/db/user.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"database/sql"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CreateUser(db *sql.DB, username, password string) error {
+
+	q := `INSERT INTO users (username, password) VALUES (?, ?)`
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(q, username, string(passwordHash))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AuthenticateUser(db *sql.DB, username, password string) (int64, error) {
+
+	q := `SELECT id, password FROM users WHERE username = ?`
+
+	var id int64
+	var storeHash string
+
+	err := db.QueryRow(q, username).Scan(&id, &storeHash)
+	if err != nil {
+		return 0, nil
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storeHash), []byte(password))
+	if err != nil {
+		return 0, nil 
+	}
+
+	return id, nil
+}
